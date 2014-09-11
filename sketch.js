8b0862bf3fcecb5329b03e233fb80db61a67cdc0
godaeun <gowooda@naver.com>
2014-09-11 13:34:26 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+  
+  // 0 : black, 255 : white
+  //background(red, gree, blue);
+  background(255, 0, 0);
   stroke(255);
   noFill();
+
   line(10, 10, 90, 10);
-    rect(20, 20, 80, 50);
-    ellipse(50, 50, 50, 50);
+  rect(20, 20, 80, 50);
+  ellipse(50, 50, 50, 50);
 }
