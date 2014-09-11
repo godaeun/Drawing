150fcf226c1fdb31a3ba2e85c8dc56c1372abed9
godaeun <gowooda@naver.com>
2014-09-11 13:37:46 +0900

    f

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ function setup() {
   // 0 : black, 255 : white
   //background(red, gree, blue);
   background(255, 0, 0);
+  
   stroke(255);
   noFill();
 
   line(10, 10, 90, 10);
-  rect(20, 20, 80, 50);
-  ellipse(50, 50, 50, 50);
+  rect(20, 20, 100, 100);
+  ellipse(320, 240, 100, 50);
 }
